e765009ffce290ceb658df1aa178203ace53c13c
Oliver9579 <55396576+Oliver9579@users.noreply.github.com>
2025-12-28 22:09:57 +0100

    Initial project structure

---
+++ backend/src/main/java/com/sztoliver/rikiki/RikikiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sztoliver.rikiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikikiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RikikiBackendApplication.class, args);
+	}
+
+}
